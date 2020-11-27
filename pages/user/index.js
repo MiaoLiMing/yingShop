a8975e2e4f63a514b0158e0a1f97fe01a4b903f3
miaoliming <l>
2020-11-27 18:28:46 +0800

    个人中心

--- pages/user/index.js
+++ pages/user/index.js
@@ -1,2 +1,36 @@
 Page({
-});
+    data: {
+        background: [
+            "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3074462172,3531747313&fm=26&gp=0.jpg",
+            "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2371856254,1139469663&fm=26&gp=0.jpg",
+            "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2500849016,1857288125&fm=26&gp=0.jpg",
+        ],
+        indicatorDots: true,
+        vertical: false,
+        autoplay: true,
+        interval: 2000,
+        duration: 1000,
+        serveList: [
+            {
+                icon: "icon-qiaquan",
+                title: "我的卡券",
+            },
+            {
+                icon: "icon-huo",
+                title: "我的活动",
+            },
+            {
+                icon: "icon-jifen",
+                title: "积分商城",
+            },
+            {
+                icon: "icon-pengyou",
+                title: "帮助服务",
+            },
+            {
+                icon: "icon-kefu",
+                title: "联系客服",
+            },
+        ],
+    },
+})
